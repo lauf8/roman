60506aec91846b4582730f013edbbf4f47a8041f
Lucas Afonso <56092251+lauf8@users.noreply.github.com>
2024-07-26 15:24:54 -0300

    docs: comments

--- int_to_roman.py
+++ int_to_roman.py
@@ -17,11 +17,11 @@ class SolutionIntToRoman:
         }
         list_roman = []
 
-        for int_num, roman in dict_roman.items():
-            while num >= int_num:
-                num = num - int_num
-                list_roman.append(roman)
+        for int_num, roman in dict_roman.items(): #percorrer os itens do dict
+            while num >= int_num: #enquanto o numero que foi mandando for maior que o numero do dict vai rodar a condição
+                num = num - int_num # retirar o valor que virou romano
+                list_roman.append(roman) # adiciona a lista o valor romano
         
-        return "".join(list_roman)
+        return "".join(list_roman) #retorma uma lista com valor convertido
         
         
--- roman_to_int.py
+++ roman_to_int.py
@@ -12,15 +12,15 @@ class SolutionRomanToInt:
         lis_s = list(s)
         tam = len(lis_s) - 1
         list_resul = []
-        for x in lis_s:
-            for chave, valor in dic.items():
-                if chave == x:
-                    list_resul.append(valor)
-                    continue
+        for x in lis_s:# for para percorrer os valores romanos que foram mandados
+            for chave, valor in dic.items(): #for para percorrer os itens do dicionário
+                if chave == x: #vefica se a chave é igual
+                    list_resul.append(valor) # achando a chave adicona o valor a list_resul
+                    continue #skipa a linha e vai para o proximo algarismo
 
-        for i in range(tam):
-            if i == tam:
+        for i in range(tam): #for para fazer as subtrações
+            if i == tam: # ignora o ultimo elemento já que ele nunca vai ser subtraido
                 continue
-            if list_resul[i] < list_resul[i+1]:
+            if list_resul[i] < list_resul[i+1]: #faz a verificação se vai acontecer a subtração
                 list_resul[i] = list_resul[i] * -1
         return sum(list_resul)
